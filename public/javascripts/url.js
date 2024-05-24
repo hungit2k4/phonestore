8ba7fdf657340941bd3d36e0ce7f969a68f5bfdf
hungit2k4 <130301063+hungit2k4@users.noreply.github.com>
2024-05-25 01:21:14 +0700

    Update url.js

--- public/javascripts/url.js
+++ public/javascripts/url.js
@@ -1,3 +1,4 @@
-export const API_URL = 'https://phonestore-70pp.onrender.com';
+//export const API_URL = 'https://phonestore-70pp.onrender.com';
 //export const API_URL = 'http://localhost:3000';
 //export const API_URL = 'https://reimagined-space-carnival-976p4wvq67w2p5j5-3000.app.github.dev';
+export const API_URL ='https://phonestore-beta.vercel.app';
